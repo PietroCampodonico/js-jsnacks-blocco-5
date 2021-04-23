13d381236820757a5a0a664f04b8362a5ba00f34
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-23 14:26:33 +0200

    Inizializzazione progetto

---
+++ js/script01.js
@@ -0,0 +1,76 @@
+/*[Snack 1]
+Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà,
+peso e lunghezza.Calcola quanto pesano tutte le zucchine.*/
+
+var listaOrtaggi = [
+    zucchina1 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina2 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina3 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina4 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina5 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina6 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina7 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina8 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina9 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+    zucchina10 = {
+        varietà: sdasdada,
+        peso: "200",
+        unita_misura_peso: g,
+        lunghezza: 10,
+        unita_misura_lunghezza: cm,
+    },
+]
